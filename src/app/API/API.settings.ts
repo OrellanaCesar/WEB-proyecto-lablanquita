8deb2fe908850e6d3a58842f962d6ebc7e3d5982
OrellanaCesar <orellanacesar.1992@gmail.com>
2021-02-08 20:44:37 -0300

    mejorando el precio

--- src/app/API/API.settings.ts
+++ src/app/API/API.settings.ts
@@ -1,5 +1,5 @@
 export class ApiSettigns {
 
-	public static url:string = 'https://softwaresalta.com/API-BLANQUITA/public/api/';
-	public static url_image =  'https://softwaresalta.com/API-BLANQUITA/storage/app/public';
+	public static url:string = 'https://softwaresalta.com/API-BLANQUITA2/public/api/';
+	public static url_image =  'https://softwaresalta.com/API-BLANQUITA2/storage/app/public';
 }
